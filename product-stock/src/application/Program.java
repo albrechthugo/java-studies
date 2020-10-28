a9b01ae05d80ff7ac26a014b17e1ab1c1736775e
Hugo <albrechthugo4@gmail.com>
2020-10-28 20:17:29 -0300

    initial commit | create principal program

---
+++ product-stock/src/application/Program.java
@@ -0,0 +1,42 @@
+package application;
+
+import entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+  public static void main(String[] args) {
+    Locale.setDefault(Locale.US);
+
+    Scanner scan = new Scanner(System.in);
+
+    double result;
+    int quantity;
+
+    Product product;
+    product = new Product();
+
+    System.out.println("Enter product data");
+
+    System.out.print("Name: ");
+    product.name = scan.nextLine();
+
+    System.out.print("Price: ");
+    product.price = scan.nextDouble();
+
+    System.out.print("Quantity: ");
+    product.quantity = scan.nextInt();
+    
+    System.out.println(product);
+
+    System.out.print("Enter the number of products to be added in stock: ");
+    quantity = scan.nextInt();
+    product.AddProducts(quantity);
+
+    System.out.print("Enter the number of products to be removed from stock: ");
+    quantity = scan.nextInt();
+    product.RemoveProducts(quantity);
+
+  }
+}
